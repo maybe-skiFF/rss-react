f57d3682867b84901a3e3782ec59916e7bb70a6b
maybe-skiFF <den.skiff190@gmail.com>
2024-07-11 17:55:21 +0300

    feat: add render pagination buttons based on the number cards

--- rss-react-project/src/components/Pagination/Pagination.tsx
+++ rss-react-project/src/components/Pagination/Pagination.tsx
@@ -6,11 +6,32 @@ interface IProps {
 }
 
 const Pagination = ({ cardsData }: IProps) => {
-  console.log(cardsData);
+  const pageCount = Math.ceil(cardsData.count / 10);
+  let pageCountArr;
+
+  (function pageCountToArr(pageCount) {
+    pageCountArr = [];
+    for (let i = 1; i <= pageCount; i++) {
+      pageCountArr.push(i);
+    }
+    return pageCountArr;
+  })(pageCount);
+
+  console.log(pageCountArr);
+  // return (
+  //   <ul className={styles.paginationWrapper}>
+  //     <li className={styles.paginationItem}>1</li>
+  //     <li className={styles.paginationItem}>2</li>
+  //   </ul>
+  // );
+
   return (
     <ul className={styles.paginationWrapper}>
-      <li className={styles.paginationItem}>1</li>
-      <li className={styles.paginationItem}>2</li>
+      {pageCountArr.map(pageNum => (
+        <li key={pageNum} className={styles.paginationItem}>
+          {pageNum}
+        </li>
+      ))}
     </ul>
   );
 };
