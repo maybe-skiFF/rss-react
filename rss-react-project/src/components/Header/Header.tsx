6b66a40b897ad1af9f05d82137aca6c317f47865
maybe-skiFF <den.skiff190@gmail.com>
2024-07-11 12:26:43 +0300

    refactor: class header component to functional component

--- rss-react-project/src/components/Header/Header.tsx
+++ rss-react-project/src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { Component, ReactNode } from 'react';
 import { Search } from '../Search/Search';
 import styles from './Header.module.scss';
 import { ErrorButton } from '../ErrorButton/ErrorButton';
@@ -8,23 +7,21 @@ interface IProps {
   setInputValue: (inputValue: string) => void;
   searchButtonHandler: (searchInputValue: string) => void;
 }
-class Header extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  render(): ReactNode {
-    return (
-      <div className={styles.headerWrapper}>
-        <Search
-          searchInputValue={this.props.searchInputValue}
-          setInputValue={this.props.setInputValue}
-          searchButtonHandler={this.props.searchButtonHandler}
-        />
-        <ErrorButton />
-      </div>
-    );
-  }
-}
+const Header = ({
+  searchInputValue,
+  setInputValue,
+  searchButtonHandler,
+}: IProps) => {
+  return (
+    <div className={styles.headerWrapper}>
+      <Search
+        searchInputValue={searchInputValue}
+        setInputValue={setInputValue}
+        searchButtonHandler={searchButtonHandler}
+      />
+      <ErrorButton />
+    </div>
+  );
+};
 
 export { Header };
