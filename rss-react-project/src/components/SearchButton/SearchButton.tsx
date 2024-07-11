5d7eef1c3c28225a6ca3581dde21813b162a18b6
maybe-skiFF <den.skiff190@gmail.com>
2024-07-11 12:42:26 +0300

    refactor: class searchbutton component to functional component

--- rss-react-project/src/components/SearchButton/SearchButton.tsx
+++ rss-react-project/src/components/SearchButton/SearchButton.tsx
@@ -1,31 +1,24 @@
-import { Component, ReactNode } from 'react';
 import styles from './SearchButton.module.scss';
 
 interface IProps {
   searchInputValue: string;
   searchButtonHandler: (searchInputValue: string) => void;
 }
-class SearchButton extends Component<IProps> {
-  constructor(props: IProps) {
-    super(props);
-  }
-
-  setSearchInputValueToLocalStorage = () => {
-    localStorage.setItem('searchInputValue', this.props.searchInputValue);
-    this.props.searchButtonHandler(this.props.searchInputValue);
+const SearchButton = ({ searchInputValue, searchButtonHandler }: IProps) => {
+  const setSearchInputValueToLocalStorage = () => {
+    localStorage.setItem('searchInputValue', searchInputValue);
+    searchButtonHandler(searchInputValue);
   };
 
-  render(): ReactNode {
-    return (
-      <button
-        onClick={this.setSearchInputValueToLocalStorage}
-        type="button"
-        className={styles.searchButton}
-      >
-        Search
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      onClick={setSearchInputValueToLocalStorage}
+      type="button"
+      className={styles.searchButton}
+    >
+      Search
+    </button>
+  );
+};
 
 export { SearchButton };
