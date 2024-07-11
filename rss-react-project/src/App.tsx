913b9ec094d943d7f86cc0f5c039b3eb0b83cf10
maybe-skiFF <den.skiff190@gmail.com>
2024-07-11 12:14:57 +0300

    refactor: class app component to functional component

--- rss-react-project/src/App.tsx
+++ rss-react-project/src/App.tsx
@@ -1,18 +1,15 @@
-import { Component, ReactNode } from 'react';
 import './App.css';
 import { MainPage } from './pages/MainPage/MainPage';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 
-class App extends Component {
-  render(): ReactNode {
-    return (
-      <>
-        <ErrorBoundary>
-          <MainPage />
-        </ErrorBoundary>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <ErrorBoundary>
+        <MainPage />
+      </ErrorBoundary>
+    </>
+  );
+};
 
 export default App;
