67589207712d81562149485b57eb67fd221e6460
maybe-skiFF <den.skiff190@gmail.com>
2024-07-11 12:16:37 +0300

    refactor: class loader component to functional component

--- rss-react-project/src/components/Loader/Loader.tsx
+++ rss-react-project/src/components/Loader/Loader.tsx
@@ -1,10 +1,7 @@
-import { Component, ReactNode } from 'react';
 import styles from './Loader.module.scss';
 
-class Loader extends Component {
-  render(): ReactNode {
-    return <h1 className={styles.loader}>Loading...</h1>;
-  }
-}
+const Loader = () => {
+  return <h1 className={styles.loader}>Loading...</h1>;
+};
 
 export { Loader };
