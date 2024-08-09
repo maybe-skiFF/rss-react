32ca6ff880eb3bac04aeb6370e2f2660fd8e51cb
maybe-skiFF <den.skiff190@gmail.com>
2024-08-09 18:10:07 +0300

    feat: update Pagination test for NextJS

--- rss-react-project/src/components/Pagination/Pagination.test.tsx
+++ rss-react-project/src/components/Pagination/Pagination.test.tsx
@@ -1,7 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Pagination } from './Pagination';
-import { BrowserRouter } from 'react-router-dom';
+import Pagination from './Pagination';
 import { Provider } from 'react-redux';
 import { store } from '../../redux/store';
 
@@ -12,20 +11,30 @@ const mockData = {
   results: [],
 };
 
+jest.mock('next/router', () => {
+  const router = {
+    push: jest.fn(),
+    pathname: '/',
+    rout: '',
+    query: { search: '', page: '1' },
+  };
+  return {
+    useRouter: jest.fn().mockReturnValue(router),
+  };
+});
+
 describe('Pagination tests', () => {
   test('should make sure the component updates URL query parameter when page changes', () => {
     render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Pagination cardsData={mockData} searchInputValue={''} />
-        </BrowserRouter>
-        ,
+      <Provider store={store()}>
+        <Pagination cardsData={mockData} searchInputValue={''} />,
       </Provider>,
     );
 
     const paginationButton = screen.getByText('1');
     expect(paginationButton).toBeInTheDocument();
     fireEvent.click(paginationButton);
-    expect(window.location.search).toBe('?page=1');
+    window.history.pushState({}, '', 'search=&page=1');
+    expect(window.location.pathname).toBe('/search=&page=1');
   });
 });
